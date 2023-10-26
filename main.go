45ea8a43dfdc0d10ea4683658edfc99b402b4926
Francis Labi <moodyblerd@.com>
2023-10-26 21:40:28 +0100

    added code

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/slack-go/slack"
@@ -11,6 +12,21 @@ func main() {
 	os.Setenv("CHANNEL_ID", "ID")
 	api := slack.New(os.Getenv("SLACK_API_TOKEN"))
 	channelArr := []string{os.Getenv("CHANNEL_ID")}
-	fileArr := []string{""}
+	fileArr := []string{"learning.png"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+
+		fmt.Printf("Name:%s\n, URL: %s\n", file.Name, file.URL)
+	}
 
 }
